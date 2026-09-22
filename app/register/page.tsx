7892a389ca0e3615058a4e01f931dcd4e50a258f
Thecorporatetrader <support@thecorporatetrader.com>
2026-09-22 21:50:09 +0530

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import GoogleIcon from '@/components/GoogleIcon';
+import { GoogleIcon } from '@/components/GoogleIcon';
 
 export default function RegisterPage() {
   const [email, setEmail] = useState('');
